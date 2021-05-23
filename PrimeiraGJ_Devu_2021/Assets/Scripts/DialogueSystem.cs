63d625950178d9daf7ade1d78dac8dda7135c75c
MatheusCapucho <matheusfcapucho@gmail.com>
2021-05-23 20:13:35 -0300

    Scripts_Iniciais
    Dialogo, Interactable, luz, URP

---
+++ PrimeiraGJ_Devu_2021/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    private Queue<string> frases;
+
+    public Animator anim;
+
+    void Start()
+    {
+        frases = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        anim.SetBool("Pop", true);
+
+        frases.Clear();
+
+        nameText.text = dialogue.unitName;
+
+        foreach (string frase in dialogue.frases)
+        {
+            frases.Enqueue(frase);
+        }
+
+        NextFrase();
+
+    }
+    public void NextFrase()
+    {
+        if (frases.Count == 0)
+        {
+            EndConversation();
+            return;
+        }
+
+        dialogueText.text = frases.Dequeue();
+    }
+    public void EndConversation()
+    {
+        anim.SetBool("Pop", false);
+    }
+}
---
+++ PrimeiraGJ_Devu_2021/Assets/Scripts/MouseInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInput : MonoBehaviour
+{
+    public Camera cam;
+    public Transform mouseGO;
+    public Vector2 mouse;
+    
+    void Update()
+    {
+        mouse = cam.ScreenToWorldPoint(Input.mousePosition);
+        mouseGO.position = mouse;       
+    }
+}
