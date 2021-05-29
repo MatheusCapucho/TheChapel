5daf913ad2f7e1c19299d8375d552c5800a493a1
IkaroSiqueira <ikaro.siqueira1@gmail.com>
2021-05-29 12:27:19 -0300

    20

--- PrimeiraGJ_Devu_2021/Assets/Scripts/Scenes_Script/Scene3_prologue.cs
+++ PrimeiraGJ_Devu_2021/Assets/Scripts/Scenes_Script/Scene3_prologue.cs
@@ -28,7 +28,7 @@ public class Scene3_prologue : GameFlow
     {
 
         anim.SetBool("Fade", true);
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(prologueScenes);
 
     }
